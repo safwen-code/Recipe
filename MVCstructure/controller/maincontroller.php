0d4608fbdabbc41ba59cc196f2c9a324f929d299
safwen-code <safwendjebbi1234@gmail.com>
2022-01-16 00:01:58 +0100

    controller file

---
+++ MVCstructure/controller/maincontroller.php
@@ -0,0 +1,38 @@
+<?php
+require_once('model.php');
+function AllRecipes()
+{
+    $recipes = GetAllRecipe();
+    require('displayRecipes.php');
+}
+function RecipesByID($id)
+{
+    $recipe = GETRecipeById($id);
+    require('detailRecipe.php');
+}
+function DeleteById($idGet, $idPost)
+{
+    $recipe = GETRecipeById($idGet);
+    require_once('deleteRecipe.php');
+    if (isset($idPost)) {
+        deleteRecipe($idPost);
+        header('Location: index.php');
+        exit();
+    }
+}
+function UpdateByID($idGet, $idPost, $title, $recipe, $author)
+{
+    $row = GETRecipeById($idGet);
+    require_once('updateRecipe.php');
+    if (isset($idPost)) {
+        updateRecipe($idPost, $title, $recipe, $author);
+        header('Location: index.php');
+        exit();
+    }
+}
+
+function AddRecipe($title, $recipe, $author)
+{
+    require_once('add.php');
+    postRecipe($title, $recipe, $author);
+}
