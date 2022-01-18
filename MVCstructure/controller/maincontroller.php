bb78059c7f5c32d317efd360cc5d0d454be3ccbe
safwen-code <safwendjebbi1234@gmail.com>
2022-01-18 14:32:10 +0100

    update main controller to work with new class

--- MVCstructure/controller/maincontroller.php
+++ MVCstructure/controller/maincontroller.php
@@ -1,36 +1,42 @@
 <?php
-require_once('./model/model.php');
+require_once('../model/RecipeManager.php');
+
 function AllRecipes()
 {
-    $recipes = GetAllRecipe();
+    $recipeManager = new RecipeManager();
+    $recipes = $recipeManager->GetAllRecipe();
     require('./view/displayRecipes.php');
 }
 function RecipesByID($id)
 {
-    $recipe = GETRecipeById($id);
+    $recipeManager = new RecipeManager();
+    $recipe = $recipeManager->GetRecipeById($id);
     require('./view/detailRecipe.php');
 }
 function DeleteById($idGet, $idPost)
 {
-    $recipe = GETRecipeById($idGet);
+    $recipeManager = new RecipeManager();
+    $recipe = $recipeManager->GetRecipeById($idGet);
     require_once('./view/deleteRecipe.php');
     if (isset($idPost)) {
-        deleteRecipe($idPost);
+        $recipeManager->DeleteRecipe($idPost);
         header('Location: index.php');
         exit();
     }
 }
 function UpdateByID($idGet, $idPost, $title, $recipe, $author)
 {
-    $row = GETRecipeById($idGet);
+    $recipeManager = new RecipeManager();
+    $row = $recipeManager->GetRecipeById($idGet);
     require_once('./view/updateRecipe.php');
     if (isset($idPost)) {
-        updateRecipe($idPost, $title, $recipe, $author);
+        $recipeManager->updateRecipe($idPost, $title, $recipe, $author);
     }
 }
 
 function AddRecipe($title, $recipe, $author)
 {
+    $recipeManager = new RecipeManager();
     require_once('./view/add.php');
-    postRecipe($title, $recipe, $author);
+    $recipeManager->AddRecipe($title, $recipe, $author);
 }
